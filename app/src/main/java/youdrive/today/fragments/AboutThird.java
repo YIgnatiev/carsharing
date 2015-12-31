2d076fbe79f3de5dd6f546c1224bd74ed3916678
vipjoker <tajcig@ya.ru>
2015-12-31 14:25:40 +0200

    fixed starter screens

--- app/src/main/java/youdrive/today/fragments/AboutThird.java
+++ app/src/main/java/youdrive/today/fragments/AboutThird.java
@@ -1,7 +1,9 @@
 package youdrive.today.fragments;
 
+import android.app.AlertDialog;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +22,48 @@ public class AboutThird extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         b = DataBindingUtil.inflate(inflater, R.layout.fragment_about_third, container, false);
+        b.setListener(this);
+        setData();
         return b.getRoot();
     }
+
+    private void setData() {
+        b.tvMinuteTarification.setText(Html.fromHtml(getString(R.string.minute_tarrif_text)));
+        b.tvFreeParking.setText(Html.fromHtml(getString(R.string.free_parking_text)));
+        b.tvFreeFuel.setText(Html.fromHtml(getString(R.string.free_fuel_text)));
+        b.tvFreeNight.setText(Html.fromHtml(getString(R.string.free_night_text)));
+        b.tvFarRides.setText(Html.fromHtml(getString(R.string.far_rides_text)));
+    }
+
+
+    private void showTextDialog(String _message){
+        AlertDialog dialog = new AlertDialog.Builder(mActivity)
+                .setMessage(_message)
+                .setPositiveButton("OK", (num, window) -> {
+                })
+                .create();
+        dialog.show();
+    }
+
+
+
+    public void onMinuteTarrification(View view){
+        showTextDialog(getString(R.string.minute_tarification));
+    }
+    public void onFreeParking(View view){
+        showTextDialog(getString(R.string.free_parking));
+    }
+
+    public void onFuelIsPayed(View view){
+        showTextDialog(getString(R.string.fuelIsPayed));
+    }
+
+    public void onFreeNightParking(View view){
+        showTextDialog(getString(R.string.free_night_parking));
+    }
+
+    public void onFarRides(View view){
+        showTextDialog(getString(R.string.far_rides));
+    }
+
 }
